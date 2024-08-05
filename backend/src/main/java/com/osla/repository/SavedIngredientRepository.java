cbb11c78262431b1610da512ced296221856a625
iendjei <iendjei@o2.pl>
2024-08-05 23:10:32 +0200

    fix SavedIngredientRepository custom queries

--- backend/src/main/java/com/osla/repository/SavedIngredientRepository.java
+++ backend/src/main/java/com/osla/repository/SavedIngredientRepository.java
@@ -1,7 +1,8 @@
 package com.osla.repository;
 
-import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.osla.model.SavedIngredient;
@@ -11,14 +12,14 @@ import com.osla.model.SavedIngredient;
 public interface SavedIngredientRepository extends JpaRepository<SavedIngredient, Integer> { 
     public SavedIngredient findByName(String name);
 
-    @Query("SELECT MAX(orderValue)+1 FROM SAVED_INGREDIENTS")
+    @Query("SELECT MAX(ing.orderValue)+1 FROM SavedIngredient ing")
     public int getNextOrder();
 
     @Query("""
-            UPDATE SAVED_INGREDIENTS
-            SET orderValue = orderValue-1
-            WHERE orderValue > givenOrder;
+            UPDATE SavedIngredient ing
+            SET ing.orderValue = ing.orderValue-1
+            WHERE ing.orderValue > :givenOrder
             """)
-    public void decrementOrderHigherThan(int givenOrder);
+    public void decrementOrderHigherThan(@Param("givenOrder") int givenOrder);
 
 }
