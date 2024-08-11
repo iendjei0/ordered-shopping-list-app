cdcf84123593f8a2265c7e9694eb2481472d76a8
iendjei <iendjei@o2.pl>
2024-08-11 14:50:35 +0200

    add js implementations for current ingredient endpoints

--- backend/src/main/java/com/osla/model/CurrentIngredient.java
+++ backend/src/main/java/com/osla/model/CurrentIngredient.java
@@ -5,12 +5,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @ToString
 @Getter
--- backend/src/main/resources/static/js/script.js
+++ backend/src/main/resources/static/js/script.js
@@ -1,25 +1,93 @@
+//HTML elements
+const outputElement = document.querySelector(".output");
+const inputElement = document.querySelector(".input input");
+const inputButtonElement = document.querySelector(".input button")
+
+
 //HTML calls
 function getCurrentIngredients() {
   return (
     fetch("/current", {method:"GET"})
-      .then(response => response.text())
+      .then(response => {
+        if(!response.ok){
+          return response.text().then(text => { 
+            throw new Error(text || response.statusText);
+          });
+        }
+        return response.text();
+      })
+      .then(html => {
+        outputElement.innerHTML = html;
+      })
+      .catch((error) => console.log(error))
   )
 }
 
-//Rendering
-async function renderCurrentIngredients() {
-  let currentIngredientsHTML = await getCurrentIngredients();
-
-  let div = document.querySelector(".output");
-  div.innerHTML = currentIngredientsHTML;
+function addCurrentIngredient(name) {
+  return (
+    fetch(`/current/add/${name}`, {method:"POST"})
+      .then(response => {
+        if(!response.ok){
+          return response.text().then(text => { 
+            throw new Error(text || response.statusText);
+          });
+        }
+        return response.text();
+      })
+      .then(html => {
+        outputElement.innerHTML = html;
+      })
+      .catch((error) => console.log(error))
+  )
 }
 
-function fullRender() {
-  renderCurrentIngredients();
+function incrementCurrentIngredient(name) {
+  return (
+    fetch(`/current/increment/${name}`, {method:"PUT"})
+      .then(response => {
+        if(!response.ok){
+          return response.text().then(text => { 
+            throw new Error(text || response.statusText);
+          });
+        }
+        return response.text();
+      })
+      .then(html => {
+        outputElement.innerHTML = html;
+      })
+      .catch((error) => console.log(error))
+  )
+}
+
+function decrementCurrentIngredient(name) {
+  return (
+    fetch(`/current/decrement/${name}`, {method:"PUT"})
+      .then(response => {
+        if(!response.ok){
+          return response.text().then(text => { 
+            throw new Error(text || response.statusText);
+          });
+        }
+        return response.text();
+      })
+      .then(html => {
+        outputElement.innerHTML = html;
+      })
+      .catch((error) => console.log(error))
+  )
 }
 
 //Listeners
-
+inputElement.addEventListener("keydown", (event) => {
+  if(event.key === "Enter") {
+    addCurrentIngredient(inputElement.value);
+    inputElement.value = "";
+  }
+})
+inputButtonElement.addEventListener("click", () => {
+  addCurrentIngredient(inputElement.value);
+  inputElement.value = "";
+})
 
 //On start
-fullRender()
+getCurrentIngredients();
